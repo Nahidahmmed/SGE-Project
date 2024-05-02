f854cb03dc361d68bd1cdd90e53d6bd7a75c1f3d
Nahidahmmed <nahidahmmed411@gmail.com>
2024-05-02 23:23:16 +0600

    Latest news section is good to go

--- src/Sections/LatestNews/LatestNews.jsx
+++ src/Sections/LatestNews/LatestNews.jsx
@@ -1,33 +1,23 @@
-import './LatestNews.css'
+import "./LatestNews.css";
+import { PiStarFourFill } from "react-icons/pi";
 
 export default function LatestNews() {
   return (
-    <div className='bg-[#4BA1FF] py-[60px]'>
-        <div className="marquee-container overflow-hidden bg-white origin-bottom -rotate-2">
-      <div className="marquee-content flex space-x-4 animate-marquee">
-        <div className="whitespace-nowrap">
-          <p className='text-5xl'>Dashboard <span></span> Wireframe <span></span> User ReseArch <span></span> UX Design</p>
-        </div>
-        <div className="whitespace-nowrap">
-          <p className='text-5xl'>Dashboard <span></span> Wireframe <span></span> User ReseArch <span></span> UX Design</p>
-        </div>
-        <div className="whitespace-nowrap">
-          <p className='text-5xl'>Dashboard <span></span> Wireframe <span></span> User ReseArch <span></span> UX Design</p>
-        </div>
-        <div className="whitespace-nowrap">
-          <p className='text-5xl'>Dashboard <span></span> Wireframe <span></span> User ReseArch <span></span> UX Design</p>
-        </div>
-        <div className="whitespace-nowrap">
-          <p className='text-5xl'>Dashboard <span></span> Wireframe <span></span> User ReseArch <span></span> UX Design</p>
-        </div>
-        <div className="whitespace-nowrap">
-          <p className='text-5xl'>Dashboard <span></span> Wireframe <span></span> User ReseArch <span></span> UX Design</p>
-        </div>
-        <div className="whitespace-nowrap">
-          <p className='text-5xl'>Dashboard <span></span> Wireframe <span></span> User ReseArch <span></span> UX Design</p>
+    <div className="bg-[#4BA1FF] py-[60px]">
+      <div className="marquee-container overflow-hidden bg-white origin-bottom -rotate-2">
+        <div className="marquee-content flex">
+          {[...Array(5)].map((_, index) => (
+            <div className="whitespace-nowrap animate-marquee py-3" key={index}>
+              <p className="text-5xl flex items-center ">
+                Dashboard <PiStarFourFill className="mx-6 text-[#15C5CE]" />
+                Wireframe <PiStarFourFill className="mx-6 text-[#15C5CE]" />
+                User ReseArch <PiStarFourFill className="mx-6 text-[#15C5CE]" />
+                UX Design <PiStarFourFill className="mx-6 text-[#15C5CE]" />
+              </p>
+            </div>
+          ))}
         </div>
       </div>
-    </div>
     </div>
   );
 }
