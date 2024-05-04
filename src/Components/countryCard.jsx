024e54a2219f974c35c15de486440fa38eaecb97
Nahidahmmed <nahidahmmed411@gmail.com>
2024-05-04 13:30:38 +0600

    working on destination section

--- src/Components/countryCard.jsx
+++ src/Components/countryCard.jsx
@@ -1,11 +1,13 @@
-export default function CountryCard() {
+import { MdArrowForwardIos } from "react-icons/md";
+// eslint-disable-next-line react/prop-types
+export default function CountryCard({image,name}) {
   return (
-    <div>
-      <div className="bg-sky-700 rounded-2xl shadow-sm shadow-sky-500 w-56 h-80 ">
-        <div className="w-56 h-80 group overflow-hidden after:duration-500 before:duration-500  duration-500 hover:duration-500 absolute  hover:rotate-[20deg] flex justify-center items-center origin-bottom  bg-neutral-900 rounded-2xl hover:pt-10 hover:ml-5 hover:mt-16 hover:bg-orange-400">
+    <div className="group ">
+      <div className=" rounded-2xl shadow-sm shadow-sky-500 w-[270px] h-[350px] group-hover:-rotate-[12deg] origin-bottom after:duration-500 before:duration-500  duration-500 group-hover:duration-500"  style={{ backgroundImage: `url(${image})` }}>
+        <div className="w-[270px] h-[350px] overflow-hidden after:duration-500 before:duration-500  duration-500 group-hover:duration-500 absolute group-hover:rotate-[20deg] flex justify-center items-center origin-bottom  bg-[#081831] rounded-2xl group-hover:pt-16 group-hover:ml-5 group-hover:mt-16 group-hover:bg-orange-400">
           <div className="z-10 flex flex-col items-center gap-2 ">
-            <span className="text-slate-400 text-6xl font-bold">JR </span>
-            <p className="text-gray-50">Frontend developer</p>
+            <span className="text-center bg-gradient-to-r from-[#88F3D0] to-[#88F3D0] bg-clip-text text-transparent text-6xl font-bold">{name} </span>
+            <button className="bg-[#2563EB] p-3 text-2xl text-white rounded-full"><MdArrowForwardIos/></button>
           </div>
         </div>
       </div>
--- src/Sections/Destination/Destination.jsx
+++ src/Sections/Destination/Destination.jsx
@@ -1,10 +1,65 @@
-
-
+import CountryCard from "../../Components/CountryCard";
 export default function Destination() {
   return (
-    <div className="w-full h-screen" style={{background: 'rgb(50,137,252)',
-        background: 'radial-gradient(circle, rgba(50,137,252,1) 0%, rgba(80,127,231,1) 0%, rgba(7,38,231,1) 66%)'}}>
-
+    <div
+      className="w-full h-[1440px]"
+      style={{
+        background: "rgb(50,137,252)",
+        // eslint-disable-next-line no-dupe-keys
+        background:
+          "radial-gradient(circle, rgba(50,137,252,1) 0%, rgba(80,127,231,1) 0%, rgba(7,38,231,1) 66%)",
+      }}
+    >
+      <div className="max-w-screen-xl mx-auto relative">
+        <img
+          className="absolute w-[240px] right-0 -top-20"
+          src="https://i.ibb.co/27Nwnrq/plane.png"
+          alt=""
+        />
+        <img
+          className="absolute lg:block md:block hidden right-9 top-[130px] w-full h-[1440px]"
+          src="https://i.ibb.co/HGtGDcP/Fligh-Path.png"
+          alt=""
+        />
+        <div className="text-center py-36">
+          <p className="bg-gradient-to-r from-[#88F3D0] to-[#88F3D0] bg-clip-text text-transparent text-6xl font-bold">Choose Your Destination</p>
+          <p className="text-white text-[76px] font-bold pt-8">We’ll Plan the Rest</p>
+        </div>
+        <div className="grid grid-cols-3 gap-11">
+          {countrys.map((country, index) => (
+            <div key={index} className="mx-auto">
+              <CountryCard image={country.image} name={country.name} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
+
+const countrys = [
+  {
+    image: "https://i.ibb.co/rG4FFM7/UK.png",
+    name: "UK",
+  },
+  {
+    image: "https://i.ibb.co/L1qXZy1/USA.png",
+    name: "USA",
+  },
+  {
+    image: "https://i.ibb.co/PxzH80z/canada.png",
+    name: "Canada",
+  },
+  {
+    image: "https://i.ibb.co/FW9JbQP/australia.png",
+    name: "Australia",
+  },
+  {
+    image: "https://i.ibb.co/8s4B9fr/Germany.png",
+    name: "Germany",
+  },
+  {
+    image: "https://i.ibb.co/WzwnCsW/New-Zealand.png",
+    name: "New Zealand",
+  },
+];
