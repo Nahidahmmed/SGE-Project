ae335e889de477565ee6cc2052554f692f910e6c
Nahidahmmed <nahidahmmed411@gmail.com>
2024-05-04 20:41:59 +0600

    Working on footer

--- src/Sections/Footer.jsx
+++ src/Sections/Footer.jsx
@@ -1,7 +1,13 @@
-
+import Footer_Head from '../../public/footer-head.png';
+import city from '../../public/background-image.png';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+      <img className='w-full' src={Footer_Head} alt="" />
+      <div className='bg-[#081831] '>
+        <img className='pt-72 w-full' src={city} alt="" />
+      </div>
+    </div>
   )
 }
