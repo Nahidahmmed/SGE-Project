126923f979e9275324b40f89bad593e3b4138c92
Nahidahmmed <nahidahmmed411@gmail.com>
2024-05-05 00:10:06 +0600

    footer is good to go

--- src/Sections/ConnectUs/ConnectUs.jsx
+++ src/Sections/ConnectUs/ConnectUs.jsx
@@ -2,7 +2,7 @@ export default function ConnectUs() {
   return (
     <div className="w-full py-[10%]">
       <div
-        className="lg:w-[1150px] md:w-[1150px] rounded-[40px] mx-auto h-[300px] bg-no-repeat relative"
+        className="lg:w-[1150px] md:w-[1150px] rounded-[40px] h-[300px] bg-no-repeat relative lg:mx-auto md:mx-auto mx-5 lg:p-0 md:p-0 p-2 bg-top"
         style={{
           backgroundImage: `url(https://i.ibb.co/m8GTzjY/Rectangle.png)`,
         }}
@@ -13,12 +13,12 @@ export default function ConnectUs() {
           alt=""
         />
         <div className="text-center text-white lg:pt-16 md:pt-16 pt-7">
-          <p className="font-bold text-5xl">Ready to take the leap?</p>
-          <p className="lg:text-2xl md:text-2xl text-xl font-[300] lg:w-[45%] md:w-[45%] w-full mx-auto mb-7 mt-3">
+          <p className="font-bold lg:text-5xl md:text-5xl text-[42px] leading-[3rem]">Ready to take the leap?</p>
+          <p className="lg:text-2xl md:text-2xl text-lgl font-[300] lg:w-[45%] md:w-[45%] w-full mx-auto mb-7 mt-3">
             Connect With Our Finest Counsellors And Biggest Study Abroad
             Community
           </p>
-          <button className="lg:text-xl md:text-xl text-lg bg-white rounded-full border-transparent border-[1px] text-black lg:px-20 md:px-20 px-3 lg:py-3 md:py-3 py-1 hover:text-blue-500 hover:border-blue-500 hover:border-[1px] ">
+          <button className="lg:text-xl md:text-xl text-lg bg-white rounded-full border-transparent border-[1px] text-black lg:px-20 md:px-20 px-4 lg:py-3 md:py-3 py-1 hover:text-blue-500 hover:border-blue-500 hover:border-[1px] ">
             Talk to a Counsellor
           </button>
         </div>
