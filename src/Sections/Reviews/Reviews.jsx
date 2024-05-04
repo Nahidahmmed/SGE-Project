096dde314f7e226c354e79cff7394923fb7f7b6a
Nahidahmmed <nahidahmmed411@gmail.com>
2024-05-04 09:52:34 +0600

    still working on review section

--- src/Sections/Reviews/Reviews.jsx
+++ src/Sections/Reviews/Reviews.jsx
@@ -1,30 +1,30 @@
 import { useState } from "react";
-import { GoArrowRight, GoArrowLeft } from "react-icons/go";
+
 export const Reviews = () => {
   const [currentSlider, setCurrentSlider] = useState(0);
   const sliders = [
     {
-      img: "https://i.ibb.co/C8cyrbh/naima.png",
+      img: "https://source.unsplash.com/1200x540/?nature",
       title: "Escape 1",
       des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
     },
     {
-      img: "https://i.ibb.co/XzYcyBg/jakaria.png",
+      img: "https://source.unsplash.com/1200x540/?hill",
       title: "Escape 2",
       des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
     },
     {
-      img: "https://i.ibb.co/C8cyrbh/naima.png",
+      img: "https://source.unsplash.com/1200x540/?mountain",
       title: "Escape 3",
       des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
     },
     {
-      img: "https://i.ibb.co/XzYcyBg/jakaria.png",
+      img: "https://source.unsplash.com/1200x540/?river",
       title: "Escape 4",
       des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
     },
     {
-      img: "https://i.ibb.co/C8cyrbh/naima.png",
+      img: "https://source.unsplash.com/1200x540/?sea",
       title: "Escape 5",
       des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
     },
@@ -39,54 +39,87 @@ export const Reviews = () => {
     );
   const isSmallScreen = window.innerWidth <= 768;
   return (
-    <div className="w-full h-60 sm:h-96 md:h-[540px] flex flex-col xl:flex-row items-center justify-center gap-5 lg:gap-10 relative bg-cover before:absolute bg-gradient-to-r from-[#031EE5] to-[#3389FD] before:inset-0 transform duration-1000 ease-linear  overflow-hidden mt-10">
-      <div className="w-1/2 px-4 drop-shadow-lg">
-        <h1 className="text-6xl font-bold mb-3 bg-gradient-to-r from-[#88F3D0] to-[#88F3D0] bg-clip-text text-transparent w-[40%]">
-          Become a Part of Our Success Stories
-        </h1>
-        <div className="flex gap-3 px-5">
+    <div
+      className="w-full h-60 sm:h-96 md:h-[540px] flex flex-col xl:flex-row items-center justify-center gap-5 lg:gap-10 relative bg-cover before:absolute bg-gradient-to-r from-[#031EE5] to-[#3389FD] before:inset-0 transform duration-1000 ease-linear z-50 overflow-hidden"
+    >
+      {/* arrow */}
+      <div className="absolute bottom-1/4 flex gap-3 z-50 px-5">
         {/* arrow left */}
         <button
           onClick={prevSlider}
-          className="flex justify-center items-center text-white border-white border-[2px] text-5xl hover:bg-white rounded-full p-2"
+          className="flex justify-center items-center hover:bg-white/30 rounded-full w-6 h-6 md:w-8 md:h-8"
         >
-          <GoArrowLeft />
+          <svg
+            viewBox="0 0 1024 1024"
+            className="w-4 h-4 md:w-6 md:h-6 icon"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#000000"
+          >
+            <g strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                fill="#0095FF"
+                d="M685.248 104.704a64 64 0 010 90.496L368.448 512l316.8 316.8a64 64 0 01-90.496 90.496L232.704 557.248a64 64 0 010-90.496l362.048-362.048a64 64 0 0190.496 0z"
+              ></path>
+            </g>
+          </svg>
         </button>
         {/* arrow right */}
         <button
           onClick={nextSlider}
-          className="flex justify-center items-center text-white border-white border-[2px] text-5xl hover:bg-white rounded-full p-2"
+          className="flex justify-center items-center hover:bg-white/30 rounded-full w-6 h-6 md:w-8 md:h-8"
         >
-          <GoArrowRight />
+          <svg
+            viewBox="0 0 1024 1024"
+            className="w-4 h-4 md:w-6 md:h-6 icon"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#000000"
+            transform="rotate(180)"
+          >
+            <g strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                fill="#0095FF"
+                d="M685.248 104.704a64 64 0 010 90.496L368.448 512l316.8 316.8a64 64 0 01-90.496 90.496L232.704 557.248a64 64 0 010-90.496l362.048-362.048a64 64 0 0190.496 0z"
+              ></path>
+            </g>
+          </svg>
         </button>
       </div>
-      </div>
-      {/* arrow */}
-      
       {/* text container here */}
-
+      <div className="w-1/2 px-4 left-0 absolute drop-shadow-lg text-white rounded-lg">
+        <h1 className="lg:text-3xl mb-3 bg-gradient-to-r from-[#88F3D0] to-[#88F3D0] bg-clip-text text-transparent">Become a Part of Our Success Stories</h1>
+      </div>
       {/* slider container */}
       <div className="w-1/2 ml-auto overflow-hidden  absolute -right-5 lg:-right-16 z-50 px-4 py-10">
         <div
           className="ease-linear duration-300 flex gap-4 items-center"
           style={{
             transform: `translateX(-${
-              currentSlider * (isSmallScreen ? 200 : 300)
+              currentSlider * (isSmallScreen ? 98 : 200)
             }px)`,
           }}
         >
           {/* sliders */}
           {sliders.map((slide, inx) => (
-            <div key={inx} className="bg-white w-[500px]">
-              <img
+            <img
+              key={inx}
               src={slide.img}
-              className={`h-[300px] w-[500px] ${
+              className={`h-[180px] sm:h-[200px] lg:h-[320px] min-w-[90px] lg:min-w-[184px] ${
                 currentSlider - 1 === inx ? "scale-0" : "scale-100 delay-500"
-              }  duration-300 rounded-lg`}
+              } drop-shadow-lg shadow-lg shadow-black bg-black/50 duration-300 rounded-lg z-50`}
               alt={slide.title}
             />
-            <p>{slide.title}</p>
-            </div>
           ))}
         </div>
       </div>
