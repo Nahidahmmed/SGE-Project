8a061a847fd0ac14fea0d9b04eca85543527611a
Nahidahmmed <nahidahmmed411@gmail.com>
2024-05-05 13:40:40 +0600

    working on review section

--- src/Sections/Reviews/Reviews.jsx
+++ src/Sections/Reviews/Reviews.jsx
@@ -1,78 +1,20 @@
 import { useState } from "react";
 
 export const Reviews = () => {
+  
   const [currentSlider, setCurrentSlider] = useState(0);
-  const sliders = [
-    {
-      img: "https://i.ibb.co/C8cyrbh/naima.png",
-      title: "Escape 1",
-      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
-    },
-    {
-      img: "https://i.ibb.co/XzYcyBg/jakaria.png",
-      title: "Escape 2",
-      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
-    },
-    {
-      img: "https://i.ibb.co/C8cyrbh/naima.png",
-      title: "Escape 3",
-      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
-    },
-    {
-      img: "https://i.ibb.co/XzYcyBg/jakaria.png",
-      title: "Escape 4",
-      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
-    },
-    {
-      img: "https://i.ibb.co/C8cyrbh/naima.png",
-      title: "Escape 5",
-      des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",
-    },
-  ];
-  const prevSlider = () =>
-    setCurrentSlider((currentSlider) =>
-      currentSlider === 0 ? sliders.length - 1 : currentSlider - 1
-    );
+  // const sliders = [{img: "https://source.unsplash.com/600x600/?bedroom", tags: "Room",}, {img: "https://source.unsplash.com/600x600/?room", tags: "Room",}, {img: "https://source.unsplash.com/600x600/?livingrooms", tags: "Room",}, {img: "https://source.unsplash.com/600x600/?livingroom", tags: "Room",}, {img: "https://source.unsplash.com/600x600/?bedrooms", tags: "Room",},];
   const nextSlider = () =>
     setCurrentSlider((currentSlider) =>
-      currentSlider === sliders.length - 1 ? 0 : currentSlider + 1
+      currentSlider === sliders.length - 2 ? 0 : currentSlider + 1
     );
-  const isSmallScreen = window.innerWidth <= 768;
   return (
-    <div
-      className="w-full h-60 sm:h-96 md:h-[540px] flex flex-col xl:flex-row items-center justify-center gap-5 lg:gap-10 relative bg-cover before:absolute bg-gradient-to-r from-[#031EE5] to-[#3389FD] before:inset-0 transform duration-1000 ease-linear z-50 overflow-hidden"
-    >
-      {/* arrow */}
-      <div className="absolute bottom-1/4 flex gap-3 z-50 px-5">
-        {/* arrow left */}
-        <button
-          onClick={prevSlider}
-          className="flex justify-center items-center hover:bg-white/30 rounded-full w-6 h-6 md:w-8 md:h-8"
-        >
-          <svg
-            viewBox="0 0 1024 1024"
-            className="w-4 h-4 md:w-6 md:h-6 icon"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="#000000"
-          >
-            <g strokeWidth="0"></g>
-            <g
-              id="SVGRepo_tracerCarrier"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            ></g>
-            <g id="SVGRepo_iconCarrier">
-              <path
-                fill="#0095FF"
-                d="M685.248 104.704a64 64 0 010 90.496L368.448 512l316.8 316.8a64 64 0 01-90.496 90.496L232.704 557.248a64 64 0 010-90.496l362.048-362.048a64 64 0 0190.496 0z"
-              ></path>
-            </g>
-          </svg>
-        </button>
-        {/* arrow right */}
+    <div className="bg-gradient-to-br from-[#3389FD] to-[#031EE5]">
+      <div className=" w-[850px] h-[540px] md:h-[670px] flex items-center relative overflow-hidden ml-auto">
+        {/* arrow */}
         <button
           onClick={nextSlider}
-          className="flex justify-center items-center hover:bg-white/30 rounded-full w-6 h-6 md:w-8 md:h-8"
+          className="absolute flex justify-center items-center top-1/2 bg-white rounded-full z-50 w-6 h-6 md:w-8 md:h-8 bgWhite right-0"
         >
           <svg
             viewBox="0 0 1024 1024"
@@ -95,34 +37,55 @@ export const Reviews = () => {
             </g>
           </svg>
         </button>
-      </div>
-      {/* text container here */}
-      <div className="w-[40%] px-4 left-0 absolute drop-shadow-lg text-white rounded-lg">
-        <h1 className="lg:text-3xl mb-3 bg-gradient-to-r from-[#88F3D0] to-[#88F3D0] bg-clip-text text-transparent">Become a Part of Our Success Stories</h1>
-      </div>
-      {/* slider container */}
-      <div className="w-[100%] ml-auto overflow-hidden  absolute -right-[1100px]  px-4 py-10">
+        {/* slider container */}
         <div
-          className="ease-linear duration-300 flex gap-4 items-center"
-          style={{
-            transform: `translateX(-${
-              currentSlider * (isSmallScreen ? 98 : 200)
-            }px)`,
-          }}
+          className="ease-linear duration-300 flex gap-[2%]"
+          style={{ transform: `translateX(-${currentSlider * 52}%)` }}
         >
           {/* sliders */}
           {sliders.map((slide, inx) => (
-            <img
-              key={inx}
-              src={slide.img}
-              className={`h-[180px] sm:h-[200px] lg:h-[320px] min-w-[90px] lg:min-w-[184px] ${
-                currentSlider - 1 === inx ? "scale-0" : "scale-100 delay-500"
-              } drop-shadow-lg shadow-lg shadow-black bg-black/50 duration-300 rounded-lg z-50`}
-              alt={slide.title}
-            />
-          ))}
+  <div
+    key={inx}
+    className={`${
+      currentSlider === inx
+        ? "bg-gradient-to-b from-[#88D6F3] via-[#88F3D0] to-[#ACFFE4] h-[260px] md:h-[530px] lg:h-[530px]"
+        : "h-[260px] md:h-[530px] lg:h-[530px]"
+    } min-w-[50%]  relative duration-200 bg-white rounded-3xl`}
+  >
+    <img src={slide.img} className="mx-auto mt-20" alt={slide.tags} />
+  </div>
+))}
+
         </div>
       </div>
     </div>
   );
 };
+
+const sliders = [
+  {
+    img: "https://i.ibb.co/C8cyrbh/naima.png",
+    name: "Naima Islam",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+  },
+  {
+    img: "https://i.ibb.co/XzYcyBg/jakaria.png",
+    name: "Jakaria Khan",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+  },
+  {
+    img: "https://i.ibb.co/C8cyrbh/naima.png",
+    name: "Naima Islam",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+  },
+  {
+    img: "https://i.ibb.co/XzYcyBg/jakaria.png",
+    name: "Jakaria Khan",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+  },
+  {
+    img: "https://i.ibb.co/C8cyrbh/naima.png",
+    name: "Naima Islam",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+  },
+];
